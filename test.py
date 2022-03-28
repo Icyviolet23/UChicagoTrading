98af607e70f38359d05f0e2c80f654d124e74c59
mingkaic1 <mingkaic@andrew.cmu.edu>
2022-03-28 00:15:00 -0400

    Tested getPredictedReturnsMatrix (to get Q, matrix of predicted returns)

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("hello")
+import numpy as np
+from allocate import *
+
+# Test getPredictedReturnsMatrix
+
+currPrices = np.array([10, 10, 10, 10, 10, 10, 10, 10, 10])
+predPrices1 = np.array([11, 12, 13, 10, 10, 10, 10, 10, 20])
+predPrices2 = np.array([12, 13, 14, 10, 10, 10, 10, 10, 20])
+predPrices3 = np.array([13, 14, 15, 10, 10, 10, 10, 10, 20])
+
+Q = getPredictedReturnsMatrix(currPrices, predPrices1, predPrices2, predPrices3)
+
+print(Q)
