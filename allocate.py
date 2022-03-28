a20f0dc98563e518d68fd0674f023f3c39c38dc4
mingkaic1 <mingkaic@andrew.cmu.edu>
2022-03-27 23:51:52 -0400

    Implemented getMarketCapWeightsVector with NumPy arrays

--- allocate.py
+++ allocate.py
@@ -7,6 +7,29 @@ import scipy
 ## for one day and allocate your portfolio accordingly.                ##
 #########################################################################
 
+# CONSTANTS
+
+SHARES_OUTSTANDING = np.array([425000000, 
+                               246970000,
+                               576250000,
+                               4230000000,
+                               1930000000,
+                               3370000000,
+                               16320000000,
+                               7510000000,
+                               508840000])
+
+# Temporary values
+HISTORICAL_COVARIANCE_MATRIX = np.array([[0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
+                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
+                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
+                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
+                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
+                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
+                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
+                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
+                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02]])
+
 def allocate_portfolio(asset_prices, asset_price_predictions_1\
                        asset_price_predictions_2,\
                        asset_price_predictions_3):
@@ -17,3 +40,10 @@ def allocate_portfolio(asset_prices, asset_price_predictions_1\
     n_assets = len(asset_prices)
     weights = np.repeat(1 / n_assets, n_assets)
     return weights
+
+# Inputs: Numpy Arrays
+def getMarketCapWeightsVector(sharesOutstanding, assetPrices):
+    totalMarketCap = np.dot(sharesOutstanding, assetPrices)
+    marketCapsVector = np.multiply(sharesOutstanding, assetPrices)
+    marketCapWeightsVector = np.true_divide(marketCapsVector, totalMarketCap)
+    return marketCapWeightsVector
