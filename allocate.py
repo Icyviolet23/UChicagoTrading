385e12fd4d25a27c5aa875a9bb9decc159496d27
mingkaic1 <mingkaic@andrew.cmu.edu>
2022-03-30 01:59:23 -0400

    allocate() function now calculates Expected Returns for 3 analysts

--- allocate.py
+++ allocate.py
@@ -11,16 +11,6 @@ import scipy
 # CONSTANTS
 # ##############################
 
-SHARES_OUTSTANDING = np.array([425000000, 
-                               246970000,
-                               576250000,
-                               4230000000,
-                               1930000000,
-                               3370000000,
-                               16320000000,
-                               7510000000,
-                               508840000])
-
 MARKET_CAP_WEIGHTS = np.array([0.00732755, 
                                0.00736718, 
                                0.01086331, 
@@ -33,17 +23,6 @@ MARKET_CAP_WEIGHTS = np.array([0.00732755,
 
 # Risk aversion coefficient
 LAMBDA = 2
-
-# Temporary values
-HISTORICAL_COVARIANCE_MATRIX = np.array([[0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
-                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
-                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
-                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
-                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
-                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
-                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
-                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02],
-                                         [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02]])
 
 # To populate with given historical data (in CSV files)
 # To update on each call of allocate_portfolio with new data
@@ -62,12 +41,6 @@ def allocate_portfolio(asset_prices,
                        asset_price_predictions_2,
                        asset_price_predictions_3):
     
-    # Test
-    DF_PRICES = pd.read_csv("Acutal Testing Data.csv")
-    DF_PREDICTED_PRICES_1 = pd.read_csv("Predicted Testing Data Analyst 1.csv")
-    DF_PREDICTED_PRICES_2 = pd.read_csv("Predicted Testing Data Analyst 2.csv")
-    DF_PREDICTED_PRICES_3 = pd.read_csv("Predicted Testing Data Analyst 3.csv")
-
     # Clean up DFs by removing first column if necessary
     # (Should only be run the first time allocate_portfolio is called)
     for DF in [DF_PRICES, 
@@ -101,49 +74,62 @@ def allocate_portfolio(asset_prices,
     bigOmega = np.diag(diagonalValues)
     print(bigOmega)
     
-    # # Calculate 3 expected returns E(R) matrices with 3 given analyst predictions
-    # ER1 = getExpectedReturns(asset_prices, asset_price_predictions_1)
-    # ER2 = getExpectedReturns(asset_prices, asset_price_predictions_2)
-    # ER3 = getExpectedReturns(asset_prices, asset_price_predictions_3)
+    # Calculate 3 expected returns E(R) matrices with 3 given analyst predictions
+    ER1 = getExpectedReturns(asset_prices,
+                             asset_price_predictions_1, 
+                             bigSigma, 
+                             bigPi, 
+                             bigOmega)
+    ER2 = getExpectedReturns(asset_prices,
+                             asset_price_predictions_2, 
+                             bigSigma, 
+                             bigPi, 
+                             bigOmega)
+    ER3 = getExpectedReturns(asset_prices,
+                             asset_price_predictions_3, 
+                             bigSigma, 
+                             bigPi, 
+                             bigOmega)
+    print("\nExpected Returns:")
+    print(ER1)
+    print(ER2)
+    print(ER3)
+
     # TO DO: Get a weighted average of the 3 E(R) values based on accuracies of analysts
     # TO DO: Iterate over some weight matrices and see which has the best Sharpe ratio
 
+# ##############################
+# HELPER FUNCTIONS
+# ##############################
+
+# Inputs: (Various)
+def getExpectedReturns(currPrices,  # List
+                       predPrices,  # List
+                       bigSigma,    # NumPy Array
+                       bigPi,       # NumPy Array
+                       bigOmega):   # NumPy Array
+    Q = getReturns(np.array(currPrices), np.array(predPrices))
+    return (np.linalg.inv(np.linalg.inv(LAMBDA * bigSigma) + np.linalg.inv(bigOmega)) 
+           @ (np.linalg.inv(LAMBDA * bigSigma) @ bigPi + np.linalg.inv(bigOmega) @ Q))
+
+# Inputs: Numpy Arrays
+def getReturns(prevPrices, nextPrices):
+    return np.log(np.true_divide(nextPrices, prevPrices))
+
+# ##############################
+# TEST
+# ##############################
+
 def test():
+    global DF_PRICES
+    global DF_PREDICTED_PRICES_1
+    global DF_PREDICTED_PRICES_2
+    global DF_PREDICTED_PRICES_3
+    DF_PRICES = pd.read_csv("Acutal Testing Data.csv")
+    DF_PREDICTED_PRICES_1 = pd.read_csv("Predicted Testing Data Analyst 1.csv")
+    DF_PREDICTED_PRICES_2 = pd.read_csv("Predicted Testing Data Analyst 2.csv")
+    DF_PREDICTED_PRICES_3 = pd.read_csv("Predicted Testing Data Analyst 3.csv")
     allocate_portfolio([1,2,3,4,5,6,7,8,9], 
                        [123,2,3,4,5,6,7,8,9],
                        [456,2,3,4,5,6,7,8,9],
                        [789,2,3,4,5,6,7,8,9])
-
-# ##############################
-# BIG HELPER FUNCTIONS
-# ##############################
-
-# def updateAllDataFrames(currPrices,
-#                         predPrices1,
-#                         predPrices2,
-#                         predPrices3):
-#     DF_PRICES.loc[len(DF_PRICES)] = currPrices
-#     DF_PREDICTED_PRICES_1[len(DF_PREDICTED_PRICES_1)] = predPrices1
-
-def getCovarianceMatrix():
-    pass
-
-def getExpectedReturns(currPrices, predPrices):
-    Q = getReturns(currPrices, predPrices)
-
-
-# ##############################
-# SMALL HELPER FUNCTIONS
-# ##############################
-
-# Inputs: Numpy Arrays
-# - Currently using logarithmic returns, to change later
-def getReturns(prevPrices, nextPrices):
-    return np.log(np.true_divide(nextPrices, prevPrices))
-
-# Inputs: Numpy Arrays
-def getMarketCapWeightsVector(sharesOutstanding, assetPrices):
-    totalMarketCap = np.dot(sharesOutstanding, assetPrices)
-    marketCapsVector = np.multiply(sharesOutstanding, assetPrices)
-    marketCapWeightsVector = np.true_divide(marketCapsVector, totalMarketCap)
-    return marketCapWeightsVector
